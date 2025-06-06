b31742526362edbf8fb4100fcb1617cf48ac5df9
Bibek Yonzan <bibek.yonzan@gmail.com>
2025-06-06 15:18:42 +0545

    old changes

--- twoDimensional/twoD_adr.m
+++ twoDimensional/twoD_adr.m
@@ -1,10 +1,17 @@
 %% 2D steady state advection diffusion
 
-close all;
-clear;
+close all
+clear
 
+x = linspace(0, 1, 10);
+y = linspace(0, 1, 10);
 
+[X, Y] = meshgrid(x, y);
 
+Z = zeros(10, 10);
+% surf(X, Y, Z)
 
-
-
+%% testing fops
+fid = fopen("test.txt", "r")
+line = fgets(fid)
+linestr = strsplit(line, ",")
